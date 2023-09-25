bd488cafac6ada985351636d9ea0d2ee153a68d6
Phil Denhoff <phil@denhoff.ca>
2023-09-24 20:54:23 -0700

    feat: api calls finance lib

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,14 +1,18 @@
 import express from 'express';
-
+import { compoundInterest } from '@org/finance';
+ 
+console.log(compoundInterest(100, 0.05, 10));
+ 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-
+ 
 const app = express();
-
+ 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+  const computed = compoundInterest(100, 0.05, 10);
+  res.send({ message: computed});
 });
-
+ 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
 });
